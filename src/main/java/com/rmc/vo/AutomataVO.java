e09160ed4c92dad8cd186120c6a0113ae98a5eaa
yinbliu <yinbliu@cn.ibm.com>
2023-09-01 09:10:34 +0800

    20230901 liuyi 2

---
+++ src/main/java/com/rmc/vo/AutomataVO.java
@@ -0,0 +1,30 @@
+package com.rmc.vo;
+
+public class AutomataVO {
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 用户状态 “未完成” “已完成”
+     */
+    private String userStatus;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
+}
